04ae8cedee78a62a7a4a9dde08def2c504c2a480
MK1331 <k2soft@mail.ru>
2022-11-18 18:55:40 +0600

    Add ListFS loader

--- boot.asm
+++ boot.asm
@@ -60,9 +60,12 @@ reboot:
 	jmp 0xFFFF:0
 ; Загрузка сектора DX:AX в буфер ES:DI
 load_sector:
+	push dx
+	add ax, word[fs_base]
+	adc dx, word[fs_base + 2]
 	cmp byte[sector_per_track], 0xFF
 	je .use_EDD
-	push bx cx dx si
+	push bx cx si
 	div [sector_per_track]
 	mov cl, dl
 	inc cl
@@ -85,10 +88,10 @@ load_sector:
 	call error
 	db "DISK ERROR",13,10,0
  @@:
-	pop si dx cx bx
+	pop si cx bx dx
 	ret
  .use_EDD:
-	push dx si
+	push si
 	mov byte[0x600], 0x10
 	mov byte[0x601], 0
 	mov word[0x602], 1
@@ -226,8 +229,12 @@ rb 510 - ($ - $$)
 db 0x55,0xAA
 ; Дополнительные данные загрузчика
 load_msg_preffix db "Loading '",0
-load_msg_suffix db "'...",0
+load_msg_suffix db "'...",13,10,0
 ok_msg db "OK",13,10,0
+config_file_name db "boot.cfg",0
+start16_msg db "Starting 16 bit kernel...",13,10,0
+start32_msg db "Starting 32 bit kernel...",13,10,0
+start64_msg db "Starting 64 bit kernel...",13,10,0
 ; Разбиение строки DS:SI по символу слеша
 split_file_name:
 	push si
@@ -235,6 +242,8 @@ split_file_name:
 	lodsb
 	cmp al, "/"
 	je @f
+	test al, al
+	jz @f
 	jmp @b
  @@:
 	mov byte[si - 1], 0
@@ -275,5 +284,111 @@ load_file:
 	ret
 ; Продолжение начального загрузчика
 boot2:
+	; Загрузим конфигурационный файл загрузчика
+	mov si, config_file_name
+	mov bx, 0x1000 / 16
+	call load_file
+	; Выполним загрузочный скрипт
+	mov bx, 0x9000 / 16
+	mov bp, 0x6000
+	mov dx, 0x1000
+ .parse_line:
+	mov si, dx
+ .parse_char:
+	lodsb
+	test al, al
+	jz .config_end
+	cmp al, 10
+	je .run_command
+	cmp al, 13
+	je .run_command
+	jmp .parse_char
+ .run_command:
+	mov byte[si - 1], 0
+	xchg dx, si
+	cmp byte[si], 0
+	je .parse_line ; Пустая строка
+	cmp byte[si], "#"
+	je .parse_line ; Комментарий
+	cmp byte[si], "L"
+	je .load_file ; Загрузка файла
+	cmp byte[si], "S"
+	je .start ; Запуск ядра
+	; Неизвестная команда
+	mov al, [si]
+	mov [.cmd], al
+	call error
+	db "Unknown boot script command '"
+	.cmd db ?
+	db "'!",13,10,0
+ .config_end: ; При правильном конфигурационном файле мы не должны сюда попасть
 	; Завершение
 	jmp reboot
+; Загрузка файла
+ .load_file:
+	push dx
+	inc si
+	call load_file
+	push ax
+	mov cx, 512
+	mul cx
+	mov word[bp + 8], ax
+	mov word[bp + 10], dx
+	mov word[bp + 12], 0
+	mov word[bp + 14], 0
+	mov ax, bx
+	mov cx, 16
+	mul cx
+	mov word[bp], ax
+	mov word[bp + 2], dx
+	mov word[bp + 4], 0
+	mov word[bp + 6], 0
+	pop ax
+	shr ax, 9 - 4
+	add bx, ax
+	add bp, 16
+	pop dx
+	jmp .parse_line
+; Запуск ядра
+ .start:
+	; Проверим, что загружен хотя бы один файл
+	cmp bx, 0x9000 / 16
+	ja @f
+	call error
+	db "NO KERNEL LOADED",13,10,0	
+ @@:
+	; Заполняем последний элемент списка файлов
+	xor ax, ax
+	mov cx, 16
+	mov di, bp
+	rep stosw
+	; Переходим к процедуре инициализации ядра для нужной разрядности
+	inc si
+	cmp word[si], "16"
+	je .start16
+	cmp word[si], "32"
+	je .start32
+	cmp word[si], "64"
+	je .start64
+	; Неизвестная рязрядность ядра
+	call error
+	db "Invalid start command argument",13,10,0
+; Запуск 16-разрядного ядра
+ .start16:
+	mov si, start16_msg
+	mov bx, 0x6000
+	mov dl, [disk_id]
+	jmp 0x9000
+; Запуск 32-разрядного ядра
+ .start32:
+	mov si, start32_msg
+	call write_str
+	;
+	jmp reboot 
+; Запуск 64-разрядного ядра
+ .start64:
+	; Выводим уведомление о запуске 64-битного ядра
+	mov si, start64_msg
+	call write_str
+	;
+	jmp reboot 

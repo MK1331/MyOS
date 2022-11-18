a62946b443681bfd474e3bbbe1005e1373ecae9e
MK1331 <k2soft@mail.ru>
2022-11-18 18:54:59 +0600

    Add ListFS loader

--- build.bat
+++ build.bat
@@ -1,1 +1,8 @@
+md bin
+md disk
+md images
 c:\fasm\fasm.exe boot.asm ./bin/boot.bios.bin
+d:\mingw\bin\gcc.exe make_listfs.c -o ./bin/make_listfs.exe
+dd if=bin/boot.bios.bin of=bin/boot_sector.bin bs=512 count=1
+dd if=bin/boot.bios.bin of=disk/boot.bin bs=1 skip=512
+.\bin\make_listfs.exe of=./images/disk.img bs=512 size=2880 boot=bin/boot_sector.bin src=./disk
